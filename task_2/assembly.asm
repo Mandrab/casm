c4c330edd80ccfa1bd58c3a125731e593085e7ef
Paolo Baldini <paolobaldini01@gmail.com>
2022-08-07 16:19:13 +0200

    shortened second task asm

--- task_2/assembly.asm
+++ task_2/assembly.asm
@@ -1,22 +1,18 @@
-    mov ecx, %[pointCount];
-    mov esi, 0xffffffff;
-    xor edi, edi;
-l1: movd mm0, %[pointSet][ecx * 4 - 4];
-    psubw mm0, %[point];
-    punpcklwd mm0, mm1;
-    pmaddwd mm0, mm0;
-    movd eax, mm0;
-    psrlq mm0, 32;
-    movd ebx, mm0;
-    add eax, ebx;
-    cmp eax, esi;
-    ja l2;
-    mov %[nearIdx], ecx;
-    dec %[nearIdx];
-    mov esi, eax;
-l2: cmp eax, edi;
-    jb l3;
-    mov %[farIdx], ecx;
-    dec %[farIdx];
-    mov edi, eax;
-l3: loop l1;
+    mov ecx, %[pointCount];             /* iterate the sequence backward (i.e., pointCount -> 0)*/
+    mov esi, 0xffffffff;                /* set initial distance of nearest point to max */
+    xor edi, edi;                       /* set initial distance of farthest point to min */
+l1: movd mm0, %[pointSet][ecx * 4 - 4]; /* take the next point from the set and load it */
+    psubw mm0, %[point];                /* individually subtract the coordinates of the target and set points */
+    pmaddwd mm0, mm0;                   /* perform Euclidean distance without sqrt (i.e., mml^2 + mmh^2) */
+    movd eax, mm0;                      /* move distance into 32bit register */
+    cmp eax, esi;                       /* check if the point is the new nearest ... */
+    ja l2;                              /* ... if it is not, go to check if it is the farthest; otherwise ... */
+    mov %[nearIdx], ecx;                /* ... save the point index */
+    dec %[nearIdx];                     /* decrease the index (ecx is decreased at the end by the loop) */
+    mov esi, eax;                       /* save the new nearest distance */
+l2: cmp eax, edi;                       /* check if the point is the new farthest ... */
+    jb l3;                              /* if it is not, go to check the next point; otherwise ... */
+    mov %[farIdx], ecx;                 /* ... save the point index */
+    dec %[farIdx];                      /* decrease the index (ecx is decreased at the end by the loop) */
+    mov edi, eax;                       /* save the new farthest distance */
+l3: loop l1;                            /* loop to check the next point */
