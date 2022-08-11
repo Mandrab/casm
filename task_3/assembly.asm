9f8c03720700ff870954aa9aa7b906136ec4a438
Paolo Baldini <paolobaldini01@gmail.com>
2022-08-11 19:57:53 +0200

    shortened third task asm

--- task_3/assembly.asm
+++ task_3/assembly.asm
@@ -1,10 +1,10 @@
     /* INITIALIZATION */
     mov ecx, %[bitCount];                       /* set number of bits to evaluate in the cycle */            
-    mov bh, byte ptr %[bitSequence];            /* move the first bits word into bh */
-    shr bh, 7;                                  /* keep only the first bit of the sequence (to not have transitions the fist cycle) */
+    mov bl, byte ptr %[bitSequence];            /* move the first bits word into bh */
 
     /* TRANSITION CHECK CYCLE */
-l0: mov eax, ecx;                               /* copy ecx to eax to perform a mask */
+l0: shl ebx, 1;                                 /* shift ebx to left; i.e., check next bits */
+    mov eax, ecx;                               /* copy ecx to eax to perform a mask */
     and eax, 0x0007;                            /* keep the lowest 3 bits of eax (i.e., remainder of eax / 8) and set ZF flag */
     jnz l1;                                     /* if there is not any reminder, continue to check the bits */
     mov eax, %[bitCount];                       /* calculate the index of the next word to load ... */
@@ -20,10 +20,10 @@ l1: mov eax, ebx;                               /* copy ebx in eax to apply a ma
     cmp eax, 0x0080;                            /* check if bits contains a 0 -> 1 transition; set ZF accordingly */
     jne l2;                                     /* if not an increment, check if it is a decrement */
     inc %[increments];                          /* increment the counter */
+    /*add %[increments], zf;*/                      /* TODO ZF = 0 if not transition, ZF = 1 otherwise */
 l2: cmp eax, 0x0100;                            /* check if bits contains a 1 -> 0 transition */
     jne l3;                                     /* if not a decrement, skip */
     inc %[decrements];                          /* increment the counter */
 
     /* SET NEW CHECK */
-l3: shl ebx, 1;                                 /* shift ebx to left; i.e., check next bits */
-    loop l0;                                    /* start the next cycle */
+l3: loop l0;                                    /* start the next cycle */
