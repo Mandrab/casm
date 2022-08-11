392b4f1df2f1c3ab50340af428745ffa57c80114
Paolo Baldini <paolobaldini01@gmail.com>
2022-08-11 19:28:09 +0200

    shortened third task asm

--- task_3/assembly.asm
+++ task_3/assembly.asm
@@ -1,12 +1,14 @@
     /* INITIALIZATION */
     xor ecx, ecx;                               /* set ecx all 0s */
-    xor ebx, ebx;
     mov bl, byte ptr %[bitSequence][ecx];       /* move the first bits word into bl */
     shr bl, 7;                                  /* keep only the highest bit of bl (needed for the first check to not have transitions) */
     mov bh, bl;                                 /* move the old word to the left (i.e., ah) */
 
     /* TRANSITION CHECK CYCLE */
-l0: mov eax, ecx;
+l0: mov eax, ecx;                               /* copy ecx to eax to perform a mask */
+    and eax, 0x0007;                            /* keep the lowest 3 bits of eax (i.e., remainder of eax / 8) and set ZF flag */
+    jne l1;
+    mov eax, ecx;
     shr eax, 3;                                 /* divide by 8 */
     mov bl, byte ptr %[bitSequence][eax];       /* move the new word into bh */
 
@@ -27,9 +29,4 @@ l2: cmp eax, 0x0100;                            /* check if bits contains a 1 ->
 l3: shl ebx, 1;                                 /* shift ebx to left; aka check next bits */
     inc ecx;                                    /* increment checked bits count */
     cmp ecx, %[bitCount];                       /* check if all the bits have been checked*/
-    je l4;                                      /* ... if not, check next */
-    mov eax, ecx;                               /* copy ecx to eax to perform a mask */
-    and eax, 0x0007;                            /* keep the lowest 3 bits of eax (i.e., remainder of eax / 8) and set ZF flag */
-    je l0;
-    jmp l1;
-l4:
+    jne l0;                                      /* ... if not, check next */
